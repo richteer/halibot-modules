616d68a7a3d6033515c076661dde802f7f7dfdfa
Benjamin Lannon <brlannon118@gmail.com>
2015-06-04 09:51:06 -0400

    Update link.py
    any trailing spaces on both sides of the title should be removed now.

--- link.py
+++ link.py
@@ -10,5 +10,5 @@ class Link(XMPPModule):
         if obj:
             addr = obj.group(1)
             webpage = requests.get(addr).content
-            title = re.match('.*<title>(.*)</title>', str(webpage)).group(1)
+            title = re.match('.*<title>(.*)</title>', str(webpage)).group(1).rstrip().lstrip()
             self.xmpp.reply(msg, "Website: " + title) 
