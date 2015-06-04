f675364e4b51134b14047a5ebce8bd3f3b4ea026
Benjamin Lannon <brlannon118@gmail.com>
2015-06-04 13:27:16 -0400

    Update link.py
    Made regexs only compile once

--- link.py
+++ link.py
@@ -1,14 +1,17 @@
 from module import XMPPModule
-import halutils
 import re, requests
- 
+
 class Link(XMPPModule):
-    
+   
+    def init(self):
+        self.addrReg = re.compile('.*(http[s]?://.*)+')
+        self.titleReg = re.compile('.*<title>(.*)</title>')
+
     def handleMessage(self, msg):
-        obj = re.match('.*(http[s]?://.*)+', msg['body'])
- 
+        obj = self.addrReg.match(msg['body'])
+
         if obj:
             addr = obj.group(1)
             webpage = requests.get(addr).content
-            title = re.match('.*<title>(.*)</title>', str(webpage)).group(1).rstrip().lstrip()
-            self.xmpp.reply(msg, "Website: " + title) 
+            title = self.titleReg.match(str(webpage)).group(1).rstrip().lstrip()
+            self.xmpp.reply(msg, "Website: " + title)
